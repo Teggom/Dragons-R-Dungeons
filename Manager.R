ec40893e1139f0d6bb5b8a5763f7855320aaa568
Stephen Kozak <stephenkozak38@gmail.com>
2019-03-01 12:29:51 -0500

    Added a user interface with the example in Manager, got ideas on how to script out the copy paste event system

--- Manager.R
+++ Manager.R
@@ -10,37 +10,54 @@ for(file in dir("Scripts/Dependant/")){
 # Run this Below code to see how this game works right now
 
 # Here is our level 1 human adventurer
+{
 Human <- Create_Functions[["Human"]]()
 
 # Here are their stats
 Print_Stats[["Human"]](Human)
-
+writeLines("\nHere you are only level 1.")
+writeLines("None of your stats are high enough to succeed...")
+writeLines("")
 # They encounter an event
 R <- Basic_Event_List[[1]](Human)
-writeLines(R[[1]])
+#writeLines(R[[1]])
+
 
 # They couldn't do anything so lets level them a bit
+writeLines("\nLets try leveling you up a few times")
+writeLines("Hopefully you will be strong enough to pass the Dexterity check")
+writeLines("Try wiggling your hand under the rock!")
+.a =readline(prompt = "Press enter to continue")
+writeLines("")
 Human <- Level_Up_Actor[["Human"]](Human)
 Human <- Level_Up_Actor[["Human"]](Human)
 Human <- Level_Up_Actor[["Human"]](Human)
 
 # Lets see those new stats 
-Print_Stats[["Human"]](Human)
+#Print_Stats[["Human"]](Human)
 
 # Try the event again
 R <- Basic_Event_List[[1]](Human)
-writeLines(R[[1]])
+#writeLines(R[[1]])
 
 # They should have had enough Dexterity to wiggle their hands under the rock
 # However it would be easier to just push the rock away if they were strong enough
+writeLines("\nSuccess! However it would be easier to push the rock aside...")
+writeLines("More levels here we go!")
+.a =readline(prompt="Press Enter to continue")
+writeLines("")
 Human <- Level_Up_Actor[["Human"]](Human)
 Human <- Level_Up_Actor[["Human"]](Human)
 Human <- Level_Up_Actor[["Human"]](Human)
 
 # Hopefully they rolled Strength above 18
-Print_Stats[["Human"]](Human)
+#Print_Stats[["Human"]](Human)
 
 # And run the event again
 R <- Basic_Event_List[[1]](Human)
-writeLines(R[[1]])
+#writeLines(R[[1]])
+
+writeLines("\nLets see the new stats as well.")
+Print_Stats[["Human"]](Human)
+}
 
--- Scripts/Dependent/Basic_Events.R
+++ Scripts/Dependent/Basic_Events.R
@@ -1,24 +1,45 @@
 Basic_Event_List <- list()
 
 .Event <- function(Human){
+  return_text <- c()
   Name <- Human[["Actor"]][["Stats"]][["Name"]]
   Text <- c(paste(Name, "came across a large rock!"))
+  Action <- 0
+  writeLines(Text)
+  return_text <- c(return_text, Text)
+  Text <- c()
+  while(!(Action %in% c(1,2,3))){
+    Action <- as.integer(readline(prompt =
+      "What would you like to do:\n[1]- Walk away.\n[2]- Feel Under the rock.\n[3]- Push the rock aside\n"
+    ))
+  }
   Actor <- Human[["Actor"]]
-  pass <- F
-  if(Actor[["Stats"]][["Strength"]] > 18){
-    Text <- c(Text, paste(Name, "pushed the rock aside."))
-    pass <- T
-  } else if(Actor[["Stats"]][["Dexterity"]]>6){
-    Text <- c(Text, paste(Name, "was able to wiggle their hand under the rock."))
-    pass <- T
+  pass = F
+  if(Action == 3){
+    Text <- c(Text, paste(Name, "tried to push the rock aside."))
+    if(Actor[["Stats"]][["Strength"]]>18){
+      Text <- c(Text, paste(Name, "pushed the rock aside."))
+      pass <- T
+    } else {
+      Text <- c(Text, paste(Name, "failed."))
+    }
+  } else if(Action == 2){
+    Text <- c(Text, paste(Name, "tried to feel under the rock."))
+    if(Actor[["Stats"]][["Dexterity"]]>6){
+      Text <- c(Text, paste(Name, "was able to wiggle their hand under the rock."))
+      pass <- T
+    } else {
+      Text <- c(Text, paste(Name, "failed."))
+    }
   }
   if(pass){
     Text <- c(Text, paste(Name, "found 24 gold coins and a ring!"))
   } else {
-    Text <- c(Text, paste(Name, "didn't know what to do so they moved on."))
+    Text <- c(Text, paste(Name, "moved on."))
   }
   # Add things to the human
-  
-  return(list(Text, Human))
+  writeLines(Text)
+  return_text <- c(return_text, Text)
+  return(list(return_text, Human))
 }
 Basic_Event_List[[length(Basic_Event_List)+1]] <- .Event 
