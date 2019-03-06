8cc38c60a693861bfe383377dcfc3c273c34026b
Stephen Kozak <stephenkozak38@gmail.com>
2019-03-06 12:31:31 -0500

    Removing more stuff in frame update, considering tkdestroy()

--- GraphicalInterfaceDebugging.R
+++ GraphicalInterfaceDebugging.R
@@ -26,16 +26,20 @@ tkpack.forget(win1$env$plot)
 tkplace(win1$env$plot, x = 0, y = 0, relx = 0, rely = 0)
 times <- c()
 Human$Actor$Stats$Current_Health = 0
-toforget <- c()
-for(frame in 1:6000){
-  toforget = c(win1$env$plot$ID, toforget)
-  if(length(toforget)>100){
-    for(each in 5:length(toforget)){
-      tkplace.forget(toforget[each])
+active_windows <- c()
+for(frame in 1:110){
+  active_windows = c(win1$env$plot$ID, active_windows)
+  if(length(active_windows)>100){
+    forgetting = active_windows[5:length(active_windows)]
+    for(each in forgetting){
+      tkplace.forget(each)
     }
-    toforget = toforget[1:4]
+    rm(list = forgetting, envir = win1$env)
+    ## ???????? tkdestroy(.x.y) ?????????
+    active_windows = active_windows[1:4]
+    #win1$env$num.subwin <- 4
   }
-  Sys.sleep(.25)
+  Sys.sleep(.1)
   .time <- Sys.time()
   Human[["Actor"]][["Stats"]][["Current_Health"]] = Human$Actor$Stats$Current_Health + 1
   #tkgrid.remove(win1$env$plot)
